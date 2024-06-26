75b13eea9b2653cf5f6cc02b6c58ee05f52cab46
Daniel Kronovet <kronovet@gmail.com>
2024-06-26 21:09:58 +0100

    Improve listener

--- src/routes/bridgexyz/listeners.ts
+++ src/routes/bridgexyz/listeners.ts
@@ -1,38 +1,46 @@
-import { ethers, type Contract } from 'ethers';
+import { ethers, type Event } from 'ethers';
 
 import data from './data.ts';
 import { type Drain } from './types.ts';
 
-const ERC20_ABI = [
+const USDC_ADDRESS = '0xC83649CC2f5488E95989Ec6d4CEc98A74793E2a7';
+const USDC_ABI = [
   'event Transfer(address indexed from, address indexed to, uint256 amount)',
 ];
 
-const listeners = {} as { [key: string]: Contract };
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+const erc20Contract = new ethers.Contract(USDC_ADDRESS, USDC_ABI, provider);
+
+const listenerSet = {} as { [key: string]: boolean };
 
 async function updateDrainListeners() {
-  const provider = new ethers.providers.JsonRpcProvider(
-    'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID',
-  );
+  console.log('Setting drain listeners...');
 
   for (const customerId of Object.keys(data.customers)) {
     for (const liquidationAddressId of Object.keys(
       data.customers[customerId].liquidation_addresses,
     )) {
-      if (!listeners[liquidationAddressId]) {
+      if (!listenerSet[liquidationAddressId]) {
         const { address } =
           data.customers[customerId].liquidation_addresses[
             liquidationAddressId
           ];
-        const erc20Contract = new ethers.Contract(address, ERC20_ABI, provider);
 
         erc20Contract.on(
           'Transfer',
-          async (from: string, to: string, amount: number) => {
+          async (from: string, to: string, amount: number, event: Event) => {
+            // Only care about token transfers to the liquidation address
+            if (to !== address) {
+              return;
+            }
+
+            const txReceipt = await event.getTransactionReceipt();
+
             const drain: Drain = {
               amount: amount.toString(),
               state: 'funds_received',
               created_at: new Date().toString(),
-              deposit_tx_hash: '0xdeadbeef',
+              deposit_tx_hash: txReceipt.transactionHash,
               receipt: {
                 destination_currency: 'usd',
                 url: 'https://dashboard.bridge.xyz/transaction/00000000/receipt/00000000',
@@ -44,7 +52,7 @@ async function updateDrainListeners() {
           },
         );
 
-        listeners[liquidationAddressId] = erc20Contract;
+        listenerSet[liquidationAddressId] = true;
       }
     }
   }
